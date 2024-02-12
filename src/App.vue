0361f73aecac6bbc134e690eb959d16f9cef2af4
Chris Pelatari <chris@bluefenix.net>
2024-02-11 22:36:59 -0600

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <v-app>
     <v-app-bar app color="primary">
       <v-app-bar-nav-icon>
-        <v-switch v-model="theme.global.name.value" inset true-value="dark" false-icon="mdi-weather-night" false-value="light" true-icon="mdi-white-balance-sunny" />
+        <v-switch v-model="theme.global.name.value" true-value="dark" false-icon="mdi-weather-night" false-value="light" true-icon="mdi-weather-sunny"/>
       </v-app-bar-nav-icon>
     </v-app-bar>
     <v-main>
