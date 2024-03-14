f45703973373101841e25029e710e467fe4fa7cd
Chris Pelatari <chris@bluefenix.net>
2024-03-13 23:07:30 -0500

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
       <v-app-bar-nav-icon>
         <v-switch v-model="theme.global.name.value" true-value="dark" false-icon="mdi-weather-night" false-value="light" true-icon="mdi-weather-sunny"/>
       </v-app-bar-nav-icon>
+      <v-toolbar-title>Insured & Bonded</v-toolbar-title>
     </v-app-bar>
     <v-main>
       <HelloWorld />
